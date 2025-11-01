412bcd4d18b160bdbf3fc6793beb2afa5aef7bfe
Anshu-priya-23 <myidanshupriya@gmail.com>
2025-11-01 11:37:53 +0530

    Initial project setup with Spring Boot

---
+++ src/main/java/com/hackathon/usedlaptopdonation/UsedLaptopDonationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.usedlaptopdonation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedLaptopDonationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedLaptopDonationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/usedlaptopdonation/UsedLaptopDonationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.usedlaptopdonation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedLaptopDonationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
