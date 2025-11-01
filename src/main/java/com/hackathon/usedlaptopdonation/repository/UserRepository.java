29886c42a2c05d98e81a118e67f3a3536623b472
padmasee05 <padmasee0511@gmail.com>
2025-11-02 02:46:33 +0530

    Removed admin-assist module files from main (moved to feature-admin-assist branch)

--- src/main/java/com/hackathon/usedlaptopdonation/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hackathon.usedlaptopdonation.repository;
-
-import com.hackathon.usedlaptopdonation.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
